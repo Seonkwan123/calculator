cb72033860e67b4906d4f0d691021825bf9139f2
Seonkwan Yoo <seonkwanyoo@gmail.com>
2023-11-27 05:38:57 -0800

    Add functions for math operators

---
+++ script.js
@@ -0,0 +1,26 @@
+let num1;
+let operator;
+let num2;
+
+function add(num1,num2) {
+    return num1 + num2;
+}
+
+function subtract(num1,num2) {
+    return num1 + num2;
+}
+
+function divide(num1,num2) {
+    return num1 / num2;
+}
+
+function multiply(num1,num2) {
+    return num1 * num2;
+}
+
+function operate(num1,operator,num2) {
+    if (operator = '+') add(num1, num2);
+    if (operator = '-') suntract(num1, num2);
+    if (operator = '/') suntract(num1, num2);
+    if (operator = '*') suntract(num1, num2);
+}
