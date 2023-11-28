9b1b73430976ed8733f77c1f42a0c07868c497f3
Seonkwan Yoo <seonkwanyoo@gmail.com>
2023-11-28 05:39:41 -0800

    Add loop to create buttons 1 thru 9

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ let num1;
 let operator;
 let num2;
 
+// Create 1 through 9 buttons using a loop
+let calculator = document.querySelector('.calculator')
+for (let i = 1; i < 10; i++) {
+    let button = document.createElement('button')
+    button.className = `${i}-button`
+    button.textContent = `${i}`
+    calculator.appendChild(button);
+}
+
 function add(num1,num2) {
     return num1 + num2;
 }
