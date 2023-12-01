f8e9c44e3febce4b90974b73661bc937af5afef4
Seonkwan Yoo <seonkwanyoo@gmail.com>
2023-12-01 11:29:55 -0800

    Add if statement to execute math

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
+const firstNumArray = [];
 const outputArray = [];
+const secondNumArray = [];
 
 let num1;
 let operator;
 let num2;
+let answer;
 
 
 
-const displayInput = document.querySelector('.displayInput')
-const displayAnswer = document.querySelector('.displayAnswer')
+const displayInput = document.querySelector('.display-input')
+const displayAnswer = document.querySelector('.display-answer')
 const buttons = document.querySelectorAll('.btn');
 
 buttons.forEach(button => {
@@ -16,9 +19,10 @@ buttons.forEach(button => {
 
 function output(e) {
     if(!isNaN(Number(e.target.innerText)) && !operator) {
-        num1 = Number(e.target.innerText);
         outputArray.push(Number(e.target.innerText));
         displayInput.innerText = outputArray.join('');
+        firstNumArray.push(Number(e.target.innerText))
+        num1 = Number(firstNumArray.join(''));
     } 
 
     if(isNaN(Number(e.target.innerText)) && e.target.innerText !== '=') {
@@ -28,18 +32,22 @@ function output(e) {
     }
 
     if(operator && !isNaN(Number(e.target.innerText))) {
-        num2 = Number(e.target.innerText);
-        output = executeMath(operator)
         outputArray.push(e.target.innerText);
         displayInput.innerText = outputArray.join('');
+        secondNumArray.push(Number(e.target.innerText))
+        num2 = Number(secondNumArray.join(''));
+    }
+    if(e.target.innerText == '=' || (isNaN(Number(e.target.innerText)) && operator && num2)) {
+        answer = executeMath(operator)
+        displayAnswer.innerText = answer;
     }
 }
 
 function executeMath(operator) {
     if (operator == '+') return add(num1,num2);
-    if (operator == '-') subtract(num1,num2);
-    if (operator == '%') divide(num1,num2);
-    if (operator == 'x') multiply(num1,num2);
+    if (operator == '-') return subtract(num1,num2);
+    if (operator == '%') return divide(num1,num2);
+    if (operator == 'x') return multiply(num1,num2);
 }
 
 
