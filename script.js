0a312f21c3b03810287fda146ecbaac1a69b5b31
Seonkwan Yoo <seonkwanyoo@gmail.com>
2023-12-01 11:05:52 -0800

    Add function to display numbers

--- script.js
+++ script.js
@@ -1,6 +1,39 @@
+const outputArray = [];
+
 let num1;
 let operator;
 let num2;
+
+const input = document.querySelector('.input')
+const answer = document.querySelector('.answer')
+const buttons = document.querySelectorAll('.btn');
+
+buttons.forEach(button => {
+    button.addEventListener('click', output)
+})
+
+function output(e) {
+    if(!isNaN(Number(e.target.innerText))) {
+        num1 = Number(e.target.innerText);
+        outputArray.push(Number(e.target.innerText));
+        input.innerText = outputArray.join('');
+    } 
+
+    if(isNaN(Number(e.target.innerText)) && e.target.innerText !== '=') {
+        operator = e.target.innerText;
+        outputArray.push(e.target.innerText);
+        input.innerText = outputArray.join('');
+    }
+
+    if(operator && !isNaN(Number(e.target.innerText))) {
+        num2 = Number(e.target.innerText);
+        outputArray.push(e.target.innerText);
+        input.innerText = outputArray.join('');
+    }
+}
+
+
+
 
 
 function add(num1,num2) {
@@ -20,8 +53,8 @@ function multiply(num1,num2) {
 }
 
 function operate(num1,operator,num2) {
-    if (operator = '+') add(num1, num2);
-    if (operator = '-') suntract(num1, num2);
-    if (operator = '/') suntract(num1, num2);
-    if (operator = '*') suntract(num1, num2);
+    if (operator == '+') add(num1, num2);
+    if (operator == '-') suntract(num1, num2);
+    if (operator == '%') divide(num1, num2);
+    if (operator == 'x') multiply(num1, num2);
 }
